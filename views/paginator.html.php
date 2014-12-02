7e7aafd9b32eaf1ed663e3fbe4fc4a497dde6f05
blair <blair@tentwentyfour.ca>
2014-12-02 12:58:47 -0600

    pagination styling; removed <a> when there is no link to go to

--- views/paginator.html.php
+++ views/paginator.html.php
@@ -4,39 +4,39 @@
 <nav class="pager no-item-preview">
     <ul>
         <li<?= (!$first_page_url) ? ' class="disabled"' : '' ?>>
-            <a href="<?= str_replace('{page}', 1, $url) ?>">
+            <?= ($first_page_url) ? '<a href="' . str_replace('{page}', 1, $url) . '">' : '<span>' ?>
                 <span aria-hidden="true">&laquo;</span>
-                    <span>
-                        <?= t("first") ?>
-                    </span>
-            </a>
+                <span>
+                    <?= t("first") ?>
+                </span>
+            <?= ($first_page_url) ? '</a>' : '</span>' ?>
         </li>
 
         <li<?= (!$previous_page_url) ? ' class="disabled"' : '' ?>>
-            <a href="<?= str_replace('{page}', $previous_page, $url) ?>">
+            <?= ($previous_page_url) ? '<a href="' . str_replace('{page}', $previous_page, $url) . '">' : '<span>' ?>
                 <span aria-hidden="true">&lsaquo;</span>
-                    <span>
-                        <?= t("previous") ?>
-                    </span>
-            </a>
+                <span>
+                    <?= t("previous") ?>
+                </span>
+            <?= ($first_page_url) ? '</a>' : '</span>' ?>
         </li>
 
         <li<?= (!$next_page_url) ? ' class="disabled"' : ''; ?>>
-            <a href="<?= str_replace('{page}', $next_page, $url) ?>">
-                <span aria-hidden="true">&rsaquo;</span>
-                    <span>
-                        <?= t("next") ?>
-                        </span>
-            </a>
+            <?= ($next_page_url) ? '<a href="' . str_replace('{page}', $next_page, $url) . '">' : '<span>' ?>
+            <span aria-hidden="true">&rsaquo;</span>
+            <span>
+                <?= t("next") ?>
+            </span>
+            <?= ($first_page_url) ? '</a>' : '</span>' ?>
         </li>
 
         <li<?= (!$last_page_url) ? ' class="disabled"' : '' ?>>
-            <a href="<?= str_replace('{page}', $last_page, $url) ?>">
+            <?= ($last_page_url) ? '<a href="' . str_replace('{page}', $last_page, $url) . '">' : '<span>' ?>
                 <span aria-hidden="true">&raquo;</span>
-                    <span>
-                        <?= t("last") ?>
-                        </span>
-            </a>
+                <span>
+                    <?= t("last") ?>
+                </span>
+            <?= ($first_page_url) ? '</a>' : '</span>' ?>
         </li>
     </ul>
 </nav>
